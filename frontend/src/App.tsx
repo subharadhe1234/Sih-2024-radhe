661a452cb106b1fde9c604b0e3d35b435af33d76
Sujit-Pal-2005 <166860736+Sujit-Pal-2005@users.noreply.github.com>
2024-09-05 02:28:29 +0530

    update app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <div className='text-3xl font-bold text-green-700'>Appraise coming soon</div>
+      <div className='text-3xl font-bold text-green-700'>Appraise</div>
     </>
   )
 }
